ebc4c2aa8dfb3cfb08178415c8f69bf48d558075
LaraCamarino <laracamarino@gmail.com>
2022-09-28 11:22:14 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+import App from "./App";
+
+import "./assets/reset.css";
+import "./assets/global.css";
+
+ReactDOM.render(<App />, document.querySelector(".root"));
